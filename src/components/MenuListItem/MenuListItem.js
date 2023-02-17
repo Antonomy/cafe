749b7125781bfdcd46da039cb32dcaa9abec00f1
Antonomy <antonyyu@gmail.com>
2023-02-17 15:40:54 -0800

    removed unnecessary concat

--- src/components/MenuListItem/MenuListItem.js
+++ src/components/MenuListItem/MenuListItem.js
@@ -3,7 +3,7 @@ import styles from './MenuListItem.module.scss'
 export default function MenuListItem ({ menuItem, handleAddToOrder }) {
   return (
     <div className={styles.MenuListItem}>
-      <div className={styles.emoji + ' ' + 'flex-ctr-ctr'}>{menuItem.emoji}</div>
+      <div className={styles.emoji + ' flex-ctr-ctr'}>{menuItem.emoji}</div>
       <div className={styles.name}>{menuItem.name}</div>
       <div className={styles.buy}>
         <span>${menuItem.price.toFixed(2)}</span>
